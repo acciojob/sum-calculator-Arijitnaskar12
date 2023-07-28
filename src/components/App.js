f30d995b3843616a9242fa6a4725d6c71370e776
ArijitNaskar12 <arijitnaskar378@gmail.com>
2023-07-29 01:06:05 +0530

    submitting code-Sat Jul 29 2023 01:05:59 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Sum from "./Sum";
 const App = () => {
+  let [sum,setSum]=useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <Sum sum={sum} setSum={setSum}/>
+          <h1>Sum:{sum}</h1>
     </div>
   )
 }
